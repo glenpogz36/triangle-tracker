9b3a0c74275c1c3504c460cefccc296945be7711
Alex Williams and glen sale <glen@>
2018-10-18 15:24:15 -0700

    update title on index.html

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,32 @@ $(document).ready(function() {
 
     if(a === b && b === c && a === c) {
       $("#equilateral").show();
+      $("#isosceles").hide();
+      $("#notriangle").hide();
+      $("#scalene").hide();
     } else if (a === b || a === c || b === c) {
+      $("#equilateral").hide();
       $("#isosceles").show();
+      $("#notriangle").hide();
+      $("#scalene").hide();
     } else if (!a || !b || !c) {
+      $("#equilateral").hide();
+      $("#isosceles").hide();
       $("#notriangle").show();
+      $("#scalene").hide();
     } else {
+      $("#equilateral").hide();
+      $("#isosceles").hide();
+      $("#notriangle").hide();
       $("#scalene").show();
 
     }
   console.log(a)
   console.log(b)
   console.log(c)
+  a = undefined;
+  b = undefined;
+  c = undefined;
   event.preventDefault();
   })
 });
